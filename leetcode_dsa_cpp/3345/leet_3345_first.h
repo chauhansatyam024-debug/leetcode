af13767842491e98770695ebb245871632fc06a9
chauhansatyam024-debug <chauhansatyam024@gmail.com>
2026-08-06 23:43:40 +0530

    update

---
+++ leetcode_dsa_cpp/3345/leet_3345_first.h
@@ -0,0 +1,30 @@
+//
+// Created by satyamchauhan on 06/08/26.
+//
+
+#ifndef LEETCODE_DSA_CPP_LEET_3345_FIRST_H
+#define LEETCODE_DSA_CPP_LEET_3345_FIRST_H
+// i used string , anyone else can use num % 10 trick
+
+// 0ms and 9.36mb , it takes some space
+#endif //LEETCODE_DSA_CPP_LEET_3345_FIRST_H
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        int g = n ;
+        while(true){ // there will be answer , so there is no need of any while condition
+            int result = 1;
+            string s  =to_string(g);
+            for(char x : s){
+                result = result * (x - '0');
+            }
+            if(result % t == 0){
+                return g;
+            }
+            else{
+                g++;
+            }
+        }
+        return g;
+    }
+};
