350fed08721b9b3f19959749d1ed0c2d95b830f8
chauhansatyam024-debug <chauhansatyam024@gmail.com>
2026-08-07 23:57:26 +0530

    update

--- leetcode_dsa_cpp/3945/leet_3945_third.h
+++ leetcode_dsa_cpp/3945/leet_3945_third.h
@@ -6,3 +6,14 @@
 #define LEETCODE_DSA_CPP_LEET_3945_THIRD_H
 
 #endif //LEETCODE_DSA_CPP_LEET_3945_THIRD_H
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        long long result = 0;
+        while(n>0){
+            result= result + n%10;
+            n = n/10;
+        }
+        return result;
+    }
+};
