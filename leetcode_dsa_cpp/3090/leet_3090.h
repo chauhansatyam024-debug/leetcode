9445171986209ccecf432c09438095c68c66b142
chauhansatyam024-debug <chauhansatyam024@gmail.com>
2026-08-15 01:19:58 +0530

    update

--- leetcode_dsa_cpp/3090/leet_3090.h
+++ leetcode_dsa_cpp/3090/leet_3090.h
@@ -10,13 +10,13 @@ class Solution {
 public:
     // sliding window
     int maximumLengthSubstring(string s) {
-        int freq[26] = {0};
+        int freq[26] = {0}; // we can use hash map , but it will hv different func or code " sort of" , logic would remain same
         int left = 0 , maxx = 0;
         for (int r = 0 ; r< s.szie() ; r++) {
             freq[s[r] - 'a']++;
             while (freq[s[r] - 'a'] > 2) {
-                freq[s[left] - 'a']--;
-                left++;
+                freq[s[left] - 'a']--; // remove the frew by 1 of left index
+                left++; // slide the window
             }
             maxx = max(maxx, (r - left) + 1); // r -left + 1 " pemdas from left to right"
         }
