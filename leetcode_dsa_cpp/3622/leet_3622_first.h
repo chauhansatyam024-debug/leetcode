991fbe414e742af154c93cc4d7a8abd2828c2300
chauhansatyam024-debug <chauhansatyam024@gmail.com>
2026-08-22 20:19:30 +0530

    update

---
+++ leetcode_dsa_cpp/3622/leet_3622_first.h
@@ -0,0 +1,27 @@
+//
+// Created by satyamchauhan on 22/08/26.
+//
+
+#ifndef LEETCODE_DSA_CPP_LEET_3622_FIRST_H
+#define LEETCODE_DSA_CPP_LEET_3622_FIRST_H
+
+#endif //LEETCODE_DSA_CPP_LEET_3622_FIRST_H
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int sum = 0;
+        int pro = 1;
+        int x = n;
+        int y ;
+        while(n!=0){
+            y = (n%10);
+            sum+=(n%10);
+            pro*=(n%10);
+            n /=10;
+        }
+        if(x % (sum + pro ) == 0){
+            return true;
+        }
+        return false;
+    }
+};
