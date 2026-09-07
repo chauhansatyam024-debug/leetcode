a54557958fb03ba70d449064503d71e53d15ce0c
chauhansatyam024-debug <chauhansatyam024@gmail.com>
2026-09-07 12:56:30 +0530

    update

--- leetcode_dsa_cpp/617/leet_617_first.h
+++ leetcode_dsa_cpp/617/leet_617_first.h
@@ -25,7 +25,7 @@ public:
         if (root1 == nullptr) return root2;
         if(root2 == nullptr) return root1;
         root1->val = root1->val + root2->val;
-        root1->left =mergeTrees(root1->left,root2->left);
+        root1->left =mergeTrees(root1->left,root2->left);  // root1->left = ,coz when nullptr in either tree come , it will not be assigned , so we hv to do that , so we can assign it
         root1->right = mergeTrees(root1->right,root2->right);
 
         return root1;
