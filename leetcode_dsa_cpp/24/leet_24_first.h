74d410ce488c74a07eea1fe9487c68f93dcdc649
chauhansatyam024-debug <chauhansatyam024@gmail.com>
2026-09-13 21:20:46 +0530

    update

---
+++ leetcode_dsa_cpp/24/leet_24_first.h
@@ -0,0 +1,43 @@
+//
+// Created by satyamchauhan on 13/09/26.
+//
+
+#ifndef LEETCODE_DSA_CPP_LEET_24_FIRST_H
+#define LEETCODE_DSA_CPP_LEET_24_FIRST_H
+
+#endif //LEETCODE_DSA_CPP_LEET_24_FIRST_H
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head ) return nullptr;
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode * temp = &dummy;
+
+        while(temp->next && temp->next->next){
+            ListNode * f = temp->next;
+            ListNode * s = f->next;
+
+
+            f->next = s->next;
+            s->next = f;
+            temp->next = s;
+
+            temp = f;
+        }
+
+
+
+        return dummy.next;
+    }
+};
